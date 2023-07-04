f99cc8814008e910c7fffd92b828305634310935
AES <ewin.sutriandi@gmail.com>
2023-07-04 12:46:45 +0800

    caesar cipher

---
+++ 05b Loop - For/for_caesar_cipher.py
@@ -0,0 +1,18 @@
+# Contoh enkripsi sederhana dengan Caesar Cipher
+abjad = ['a','b','c','d','e','f','g','h','i','j',
+         'k','l','m','n','o','p','q','r','s','t',
+         'u','v','w','x','y','z']
+cipher = 2
+teks = "teknik informatika universitas hamzanwadi"
+hasil_enkripsi = ""
+for t in teks:
+    if t != " ":
+        indeks_lama = abjad.index(t)    
+        indeks_baru = (indeks_lama + cipher) % len(abjad)
+        hasil_enkripsi = hasil_enkripsi + abjad[indeks_baru]
+    else:
+        hasil_enkripsi = hasil_enkripsi + " "
+
+print(hasil_enkripsi)
+
+
