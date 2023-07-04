1abbd75c70042ff8415cabd26fc4d5f45aa61c81
AES <ewin.sutriandi@gmail.com>
2023-07-04 09:39:29 +0800

    fizzbuzz

---
+++ 05b Loop - For/for_fizz_buzz.py
@@ -0,0 +1,27 @@
+'''
+Buatlah sebuah program yang menerima input sebuah bilangan bulat (misal: n), 
+kemudian mencetak n baris dengan pola sebagai berikut:
+Untuk n = 20
+
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+17
+Fizz
+19
+Buzz
+'''
+
--- 05b Loop - For/for_piramida2.py
+++ 05b Loop - For/for_piramida2.py
@@ -2,12 +2,18 @@
 Buatlah program yang menerima input sebuah bilangan bulat n
 kemudian mencetak piramida dengan n baris 
 seperti pada ilustrasi berikut
-    *
-   * *
-  * * * 
- * * * * 
-* * * * * 
+        *              1
+      * * *            3
+    * * * * *          5
+  * * * * * * *        7
+* * * * * * * * *      9
 dst
 '''
 n = int(input("Masukkan sebuah bilangan bulat: "))
 
+for i in range(n): # perulangan sejumlah baris yang diinginkan
+    for k in range(n-i-1):
+        print("  ",end="")
+    for j in range(i+1+i): # perulangan sejumlah bintang yg dicetak
+        print("* ", end="") # agar tidak ganti baris
+    print() # ganti baris sesuai i
