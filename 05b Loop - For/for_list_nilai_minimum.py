20a06d4809aaa98b98b30d1805ea5be3f10637b1
AES <ewin.sutriandi@gmail.com>
2023-07-04 11:43:43 +0800

    list basic

--- 05b Loop - For/for_list_nilai_minimum.py
+++ 05b Loop - For/for_list_nilai_minimum.py
@@ -3,8 +3,3 @@ nilai_uts = [35,65,78,75,22,87,90,32,85,87,65]
 
 # gunakan perulangan for untuk mencari nilai minimum pada list
 
-nil_min = nilai_uts[0]
-for nil in nilai_uts:
-    if nil < nil_min:
-        nil_min = nil
-print(nil_min) 
