5d0e57089dc376e6d3b8c74d540681ffd2c28f3c
AES <ewin.sutriandi@gmail.com>
2023-07-11 08:25:31 +0800

    +better desc

--- 05b Loop - For/for_caesar_decipher.py
+++ 05b Loop - For/for_caesar_decipher.py
@@ -1,14 +1,15 @@
 # Contoh enkripsi sederhana dengan Caesar Cipher
+# Decipher (menerjemahkan) teks yang telah dienkripsi sebelumnya
 abjad = ['a','b','c','d','e','f','g','h','i','j',
          'k','l','m','n','o','p','q','r','s','t',
          'u','v','w','x','y','z']
-cipher = -2
+cipher = 2
 teks = "vgmpkm kphqtocvkmc wpkxgtukvcu jcobcpycfk"
 hasil_enkripsi = ""
 for t in teks:
     if t != " ":
         indeks_lama = abjad.index(t)    
-        indeks_baru = (indeks_lama + cipher) % len(abjad)
+        indeks_baru = (indeks_lama - cipher) % len(abjad)
         hasil_enkripsi = hasil_enkripsi + abjad[indeks_baru]
     else:
         hasil_enkripsi = hasil_enkripsi + " "
