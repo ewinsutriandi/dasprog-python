b9d1db5353a4a05addcc6102c19d0f95196227b3
AES <ewin.sutriandi@gmail.com>
2023-07-13 09:45:13 +0800

    m

--- temp.py
+++ temp.py
@@ -1,28 +1,21 @@
 '''
-Buatlah program yang menerima dua buah bilangan bulat
-kemudian menghitung FPBnya dengan terlebih dahulu 
-mencari seluruh faktor dari masing-masing bilangan
+buatlah program yang menerima  dua buah bil bulat kemudian menghitung FPBnya dengan terlebih dahulu mencari seluruh faktor dari masing-masing bilangan.
 '''
 
-# Input bilangan
-b1 = int(input("Bil 1: "))
-b2 = int(input("Bil 2: "))
+# input bilangan
+x = int(input("Masukan bilangan pertama:"))
+y = int(input("masukan bilangan kedua:"))
 
-# Cari faktor b1 dan b2
-faktor_b1 = []
-faktor_b2 = []
-for i in range(b1,0,-1):
-    if b1 % i == 0:
-        faktor_b1.append(i)
-for i in range(b2,0,-1):
-    if b2 % i == 0:
-        faktor_b2.append(i)
-print(f"Faktor dari {b1}: {faktor_b1}")
-print(f"Faktor dari {b2}: {faktor_b2}")
-# Cari FPB
-for f in faktor_b1:
-    if f in faktor_b2:
-        fpb = f
-        break
+#   cari faktor bil dan FPB
+if x > y:
+  bil_terkecil = y
+else:
+  bil_terkecil = x
 
-print(f"FPB {b1} & {b2} adalah {f}")    
+for i in range(1,bil_terkecil+1): 
+  if ( x % i == 0 ) and (y % i == 0):
+     fpb = i
+
+# Tampilkan
+print(f" Faktor dari fpb adalh {fpb}")
+  
