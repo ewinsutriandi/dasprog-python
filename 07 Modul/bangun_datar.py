e8e2b5e96960f148a3e4785f1cdbe2c180c6b773
AES <ewin.sutriandi@gmail.com>
2023-10-18 10:18:02 +0800

    + funcs

--- 07 Modul/bangun_datar.py
+++ 07 Modul/bangun_datar.py
@@ -5,5 +5,13 @@ def luas_persegi_panjang(pjg,lbr):
     return pjg * lbr
 
 def luas_segitiga(alas,tinggi):
-    return 0.5 * alas * tinggi
+    return alas * tinggi / 2
 
+def luas_lingkaran(radius):
+    return 3.14 * radius ** 2
+
+def luas_trapesium(sisi1,sisi2,tinggi):
+    return (sisi1+sisi2)/2 * tinggi
+
+def luas_belah_ketupat(diag1,diag2):
+    return diag1 * diag2 /2
--- 07 Modul/cara_menggunakan_modul.py
+++ 07 Modul/cara_menggunakan_modul.py
@@ -1,17 +1,16 @@
-import bangun_datar  # cara 1
+import bangun_datar # cara 1
 print(bangun_datar.luas_persegi(3))
-print(bangun_datar.luas_persegi_panjang(3,4))
 
 import bangun_datar as bd # cara 2
-print(bd.luas_segitiga(4,8))
+print(bd.luas_persegi_panjang(3,4))
 
-from bangun_datar import luas_persegi # cara 3
-print(luas_persegi(7))
+from bangun_datar import luas_segitiga # cara 3
+print(luas_segitiga(4,5))
 
-from bangun_datar import luas_segitiga as l_sgtg # cara 4
-print(l_sgtg(6,5))
+from bangun_datar import luas_trapesium,luas_lingkaran
+print(luas_lingkaran(6))
+print(luas_trapesium(3,4,5))
 
-from bangun_datar import luas_persegi,luas_persegi_panjang # cara 5
-print(luas_persegi(5))
-print(luas_persegi_panjang(7,8))
+from bangun_datar import luas_persegi as lp # cara 4
+print(lp(6))
 
