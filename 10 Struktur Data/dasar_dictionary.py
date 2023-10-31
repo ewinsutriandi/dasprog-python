785b430a494257778b963c1469c66883008b5a13
AES <ewin.sutriandi@gmail.com>
2023-10-31 10:54:19 +0800

    gigi

---
+++ 10 Struktur Data/dasar_dictionary.py
@@ -0,0 +1,40 @@
+# deklarasi
+upin = {
+    "nama lengkap" : "Aruffin bin Abdul Salam",
+    "tinggi" : 110,
+    "berat"  : 17
+}
+
+mhs = {
+    "nim" : "1234567",
+    "nama" : "Asep Davidson",
+    "prodi" : "Teknik Informatika",
+    "thn masuk" : 2021
+}
+
+proglanj = {
+    "kode" : "TI6303",
+    "nama" : "Pemrograman Lanjut",
+    "sks" : 3
+}
+# akses elemen pada dictionary menggunakan key
+print(upin["nama lengkap"],upin["tinggi"])
+print(mhs["nim"],mhs["nama"],mhs["prodi"],mhs["thn masuk"])
+print(f"Mata kuliah {proglanj['nama']} memiliki bobot {proglanj['sks']} SKS")
+
+# update elemen
+mhs["prodi"] = "Ilmu Komputer"
+print(mhs)
+
+# tambah elemen 
+mhs["asal"] = "Pancor"
+print(mhs)
+
+# loop
+for k in mhs:
+    print(k,mhs[k])
+
+for k,v in mhs.items():
+    print(k,v)
+
+    
