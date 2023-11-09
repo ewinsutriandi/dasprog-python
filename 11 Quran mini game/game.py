50f67b9111e5643fb417420c69f91a65089b8695
AES <ewin.sutriandi@gmail.com>
2023-11-09 12:58:23 +0800

    refactored, func for choice construction

--- 11 Quran mini game/game.py
+++ 11 Quran mini game/game.py
@@ -1,36 +1,35 @@
 import json
 import random
 
-file = open("11 Quran mini game\surat_alquran.json",encoding="utf8") # load file
-daftar_surat = json.load(file) # konversi isi file (json) ke dictionary
+file = open("11 Quran mini game/surat_alquran.json")
+daftar_surat = json.load(file) # load json file ke dalam dictionary
 
-daftar_surat = list(daftar_surat.values()) # konversi dictionary menjadi list
-surat_10 = random.sample(daftar_surat,k=10) # ambil sampel 10 surat secara acak
+daftar_surat = list(daftar_surat.values()) # konversi dictionary ke list
+surat_terpilih = random.sample(daftar_surat, k=10) # ambil 10 surat secara acak
 
-pilihan_salah = []
-for surat in daftar_surat:
-    pilihan_salah.append(surat['arti_nama'])
+def susun_pilihan_ganda(surat):
+    pil_benar = surat['arti_nama']
+    kandidat_pilihan = []
+    for s in daftar_surat:
+        kandidat_pilihan.append(s['arti_nama'])
+    kandidat_pilihan.remove(pil_benar) # buang yg benar dari kandidat pilihan (akan ditambah kemudian)
+    pil_ganda = random.sample(kandidat_pilihan,k=2) # tambahkan dua jawaban yg salah (dari surat lain)
+    pil_ganda.append(pil_benar) # tambahkan jawaban yg benar
+    random.shuffle(pil_ganda) # acak urutan pilihan
+    peta_pilihan = {"a": pil_ganda[0], "b": pil_ganda[1], "c":pil_ganda[2] } # petakan dengan abjad a,b,c
+    return peta_pilihan 
 
 skor = 0
-for surat in surat_10:
-    print(f"Arti nama dari surat {surat['nama']} adalah?: ") # cetak pertanyaan
-    # siapkan pilihan jawaban
-    jawaban_benar = surat['arti_nama']
-    pilihan_salah.remove(jawaban_benar) # buang jawaban benar dari list
-    pilihan_jawaban = random.sample(pilihan_salah,k=2) # tambahkan jawaban salah
-    pilihan_jawaban.append(jawaban_benar) # tambahkan jawaban benar
-    random.shuffle(pilihan_jawaban) # acak urutan
-    peta_jawaban = {"a":pilihan_jawaban[0],"b":pilihan_jawaban[1],"c":pilihan_jawaban[2]}
-    # cetak pilihan jawaban
-    print(f"Pilih a. {pilihan_jawaban[0]} b. {pilihan_jawaban[1]} c. {pilihan_jawaban[2]}")
-    
-    respon_user = input("a/b/c: ") # ambil respon user (a,b,c)
-    jawaban_user = peta_jawaban[respon_user]
-
-    if jawaban_user == jawaban_benar:
+for surat in surat_terpilih:
+    print(f"Arti dari nama surat {surat['nama']} adalah?")
+    pilihan_ganda = susun_pilihan_ganda(surat)
+    print(f"a. {pilihan_ganda['a']} b. {pilihan_ganda['b']} c. {pilihan_ganda['c']}")
+    jwban = input("a/b/c?: ")
+    jwban = pilihan_ganda[jwban]
+    if jwban == surat["arti_nama"]:
         skor += 1
-        print(f"Benar! Skor anda sekarang {skor}")
+        print(f"Benar!")
     else:
-        print(f"Salah! Jawaban yang benar adalah: {jawaban_benar}")
+        print(f"Salah! Jawaban yang benar adalah {surat['arti_nama']}") 
 
-print(f"Permainan berakhir, anda mendapatkan skor akhir {skor}")
+print(f"Permainan berakhir, skor anda {skor}")
