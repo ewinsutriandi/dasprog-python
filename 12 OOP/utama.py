b2368aff4c2be34ad9c107325b986b597faf5b91
AES <ewin.sutriandi@gmail.com>
2023-12-14 09:24:05 +0800

    instance method

--- 12 OOP/utama.py
+++ 12 OOP/utama.py
@@ -4,15 +4,7 @@ from cth_class import Persegi,PersegiPanjang,Mahasiswa
 p1 = Persegi(4)
 p2 = Persegi(7)
 
-print(p1.sisi) # akses atribut dengan tanda titik setelah object
-print(p2.sisi)
+print(f"Persegi dengan sisi {p1.sisi} memiliki keliling {p1.keliling()}") 
+print(f"Persegi dengan sisi {p2.sisi} memiliki luas {p2.luas()}") 
 
-pp1 = PersegiPanjang(3,4)
-pp2 = PersegiPanjang(2,3)
-
-print(f"pp1 memiliki pjg {pp1.pjg} dan lebar {pp1.lbr}")
-
-mhs1 = Mahasiswa("12345","Upin","Informatika",2022)
-
-print(f"{mhs1.nama} adalah mahasiswa {mhs1.prodi} angkatan {mhs1.angkatan}")
 
