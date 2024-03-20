c25ffd9f65cab45c5d3f0f0b712adaf9cccffb2c
ewinsutriandi <ewin.sutriandi@gmail.com>
2024-03-20 18:09:02 +0800

    trc

--- 01 Prints/04 error_case.py
+++ 01 Prints/04 error_case.py
@@ -1,1 +1,3 @@
-Print("Hello, world!")
+print("Hello, world!")  # benar
+Print("Hello, world!")  # salah penulisan Print, seharusnya print
+PRINT("Hello, world!")  # salah penulisan PRINT, seharusnya print
---
+++ 01 Prints/05 error_kutip.py
@@ -0,0 +1,4 @@
+print("Hello, world!")  # benar
+print('Hello, world!')  # benar
+print(Hello, world!)  # salah, teks harus dikutip
+print("Hello, world!')  # salah, penggunaan tanda kutip harus konsisten
