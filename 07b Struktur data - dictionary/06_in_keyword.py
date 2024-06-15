a0ef8108f3e98ce1dbdcf87b757c06ff3abebf2a
ewinsutriandi <ewin.sutriandi@gmail.com>
2024-06-15 10:39:45 +0800

    + soal uts

--- 07b Struktur data - dictionary/06_in_keyword.py
+++ 07b Struktur data - dictionary/06_in_keyword.py
@@ -4,5 +4,5 @@ mk = {
     "sks"   : 3,
     "wajib" : True
 }
-print("nama" in mk)
-print("dosen" in mk)
+print("nama" in mk)  # menghasilkan True karena ada key nama pada mk
+print("dosen" in mk) # menghasilkan False karena tidak ada key dosen pada mk
