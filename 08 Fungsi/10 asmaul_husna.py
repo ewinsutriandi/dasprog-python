be856f8b07901778fae74eecc90707d0b1539d3b
ewinsutriandi <ewin.sutriandi@gmail.com>
2024-10-28 10:06:41 +0800

    wes

--- 08 Fungsi/10 asmaul_husna.py
+++ 08 Fungsi/10 asmaul_husna.py
@@ -13,7 +13,7 @@ def load_asmaul_husna():
     except FileNotFoundError:
         print("File tidak ditemukan")
 
-def tampilkan_petunjuk_bermain():
+def tampilkan_deksripsi_permainan():
     ''' Fungsi untuk menampilkan petunjuk bermain'''
     print("ARTI ASMAUL HUSNA")
     print("Komputer akan memilih 10 nama dari 99 asmaul husna")
@@ -23,14 +23,57 @@ def tampilkan_petunjuk_bermain():
 def tampilkan_statistik_permainan(benar,salah):
     print("PERMAINAN BERAKHIR")
     print(f"Total soal: {benar + salah}")
-    print(f"benar: {benar}, salah: {salah}")    
+    print(f"benar: {benar}, salah: {salah}")
+
+def siapkan_pilihan(nama,asmaul_husna):
+    kandidat_pilihan_salah = asmaul_husna.copy()
+    jawaban_benar = nama["Indonesia"]
+    kandidat_pilihan_salah.remove(nama) # buang jawaban benar dari kandidat jawaban salah
+    kandidat_pilihan_salah = random.sample(kandidat_pilihan_salah,2) # siapkan dua jawaban salah
+    pilihan_jawaban = [jawaban_benar] # buat list berisi jawaban benar
+    pilihan_jawaban.append(kandidat_pilihan_salah[0]['Indonesia']) # jawaban salah 1
+    pilihan_jawaban.append(kandidat_pilihan_salah[1]['Indonesia']) # jawaban salah 2
+    random.shuffle(pilihan_jawaban) # acak posisinya
+    pilihan = {
+        "a" : pilihan_jawaban[0],
+        "b" : pilihan_jawaban[1],
+        "c" : pilihan_jawaban[2],
+    }
+    return pilihan
+
+def input_jawaban():
+    jawab = False
+    while not jawab:
+        pilihan = input("Masukkan jawaban anda (a/b/c): ")
+        if pilihan.lower() not in ["a","b","c"]:
+            print("Pilihan jawaban tidak dikenali")
+        else:
+            jawab = True
+    return pilihan
 
 def mulai_permainan():
-    data = load_asmaul_husna() 
-    pilih_10_nama = random.sample(data,10)
-    for nama in pilih_10_nama:
-        print(nama['Nama'],nama['Indonesia'])
+    asmaul_husna = load_asmaul_husna()
+    jum_pertanyaan = 5
+    benar = 0
+    salah = 0
+    terpilih = random.sample(asmaul_husna,jum_pertanyaan)
     
+    for nama in terpilih:
+        pertanyaan = f"Arti dari asmaul husna: {nama["Nama"]} adalah?"
+        jawaban_benar = nama["Indonesia"]
+        pilihan = siapkan_pilihan(nama,asmaul_husna)
+        print(pertanyaan)
+        for k in pilihan:
+            print(f"{k}. {pilihan[k]}")
+        jawaban_user = input_jawaban()
+        if pilihan[jawaban_user] == jawaban_benar:
+            print("Anda benar!")
+            benar += 1
+        else:
+            salah +=1
+            print(f"Salah, jawaban yang benar adalah: {jawaban_benar}")
+        
+    tampilkan_statistik_permainan(benar,salah)        
 
 mulai_permainan()
 
